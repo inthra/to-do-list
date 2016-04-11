04fffc7f186f3c5d6a288236f3e05714fe90ee15
IM <zign.int@gmail.com>
2016-04-10 21:48:05 -0700

    added double click method on the task that can be indicated 'Done', and added remove button to remove all finished tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,25 @@
 // business logic
-var todoItems = [];
-
+function Task(inputTask) {
+  this.whatTask = inputTask;
+}
 
 // user interface logic
 $(document).ready(function(){
   $("form#blank").submit(function(event){
     event.preventDefault();
     var inputTodo = $("input#todo").val();
-    todoItems.push(inputTodo);
-    console.log(todoItems);
+    var newTodo = new Task(inputTodo);
 
+    $("ul.todo-list").append("<li>" + newTodo.whatTask + "</li>");
 
-    $(".todo-list").append("<li class='todoNow'>" + inputTodo + "</li>");
-
-    $(".todoNow").click(function(){
+    $("li").dblclick(function(){
       $(this, "li").text("Done");
+    });
+
+    $("#remove").click(function(){
+      $("li").remove(":contains('Done')")
     });
 
   $("#todo").val("");
   });
-
-
-
 });
-
-
-
-
-// *******experimental code for counting through the array*******
-// for (i=0; i <= todoItems.length; i += 1) {
-//   $(".todo-list").append("<li class='todoNow'>" + todoItems[i] + "</li>");
-//
-//   $(".todoNow").click(function(){
-//     $(this, "li").text("Done");
-//   });
-// }
